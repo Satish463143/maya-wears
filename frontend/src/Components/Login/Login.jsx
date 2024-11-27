2aa55cb09a177ea1c15c65e2aebfe52377787375
Aarush Mahato <aarushmahato4@gmail.com>
2024-11-27 16:26:15 +0545

    #changes

--- frontend/src/Components/Login/Login.jsx
+++ frontend/src/Components/Login/Login.jsx
@@ -1,96 +1,115 @@
-import React, { useEffect, useState } from 'react';
-import './Login.css'
-import { useForm } from 'react-hook-form';
-import {useNavigate} from 'react-router-dom'
-import { TextInputComponent } from '../../Middlewares/Form/Input.component'
-import { yupResolver } from '@hookform/resolvers/yup';
-import * as Yup from 'yup';
-import authSvc from '../../pages/LoginPage/auth.service'
-import { toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import { useDispatch, useSelector } from 'react-redux';
-import { setLoggedInUserForRedux } from '../../reducer/user.reducer';
+import React, { useEffect, useState } from "react";
+import "./Login.css";
+import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
+import { TextInputComponent } from "../../Middlewares/Form/Input.component";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as Yup from "yup";
+import authSvc from "../../pages/LoginPage/auth.service";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { useDispatch, useSelector } from "react-redux";
+import { setLoggedInUserForRedux } from "../../reducer/user.reducer";
 
-const Login = ({ setCurrentView,setLoggedIn}) => {
-    
-    const [loading, setLoading] = useState(false);
-    const dispatch  = useDispatch()
-    const navigate = useNavigate()
+const Login = ({ setCurrentView, setLoggedIn }) => {
+  const [loading, setLoading] = useState(false);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
+  const loginDTO = Yup.object({
+    email: Yup.string().email().required(),
+    password: Yup.string().required(),
+  });
 
-    const loginDTO = Yup.object({
-        email: Yup.string().email().required(),
-        password: Yup.string().required(),
-    });
+  const {
+    handleSubmit,
+    control,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(loginDTO),
+  });
 
-    const { handleSubmit, control, formState: { errors } } = useForm({
-        resolver: yupResolver(loginDTO),
-    });
+  const loggedInUser = useSelector((root) => {
+    return root.user.loggedInUser;
+  });
+  useEffect(() => {
+    if (loggedInUser) {
+      if (loggedInUser.role === "admin") {
+        setLoggedIn(false);
+        navigate("/admin");
+      }
+    }
+  }, [loggedInUser]);
 
+  const login = async (data) => {
+    try {
+      setLoading(true);
+      const response = await authSvc.postRequest("/auth/login/", data);
+      setLoading(false);
+      toast.success("Welcome to Maya Wears");
+      localStorage.setItem("_at", response.result.token.token);
+      localStorage.setItem("_rt", response.result.token.refreshToken);
+      dispatch(setLoggedInUserForRedux(response.result.userDetail));
+      // After successful login, get the logged-in user
+      // loggedInUser(); // Ensure the profile is shown after login
+      setLoggedIn(true);
+    } catch (exception) {
+      setLoading(false);
+      toast.error(exception.data?.message || "Login failed");
+    }
+  };
 
-    const loggedInUser = useSelector((root)=>{
-        return root.user.loggedInUser
-    })
-    useEffect(()=>{
-        if(loggedInUser){
-            if(loggedInUser.role === "admin") {
-                setLoggedIn(false)
-                navigate('/admin')                
-            } 
-        }
-    },[loggedInUser])
-
-    const login = async (data) => {
-        try {
-            setLoading(true);
-            const response = await authSvc.postRequest('/auth/login/', data);
-            setLoading(false);            
-            toast.success('Welcome to Maya Wears');
-            localStorage.setItem("_at", response.result.token.token);
-            localStorage.setItem("_rt", response.result.token.refreshToken);
-            dispatch(setLoggedInUserForRedux(response.result.userDetail))
-            // After successful login, get the logged-in user
-            // loggedInUser(); // Ensure the profile is shown after login
-            setLoggedIn(true)         
-          
-        } catch (exception) {
-            setLoading(false);
-            toast.error(exception.data?.message || 'Login failed');
-        }
-      };
-
-    
-    
   return (
-        <div className='login_box'>
-            <form action=""method="post" onSubmit={handleSubmit(login)} >
-                <div className="form_body">
-                    <label htmlFor="email">Email<span>*</span></label><br />
-                    <TextInputComponent
-                        name="email"
-                        errMsg={errors?.email?.message || null}
-                        required:true
-                        control={control}
-                    /> 
-                    <br />
-                    <label htmlFor="password">Password<span>*</span></label><br />
-                    <TextInputComponent
-                        name="password"
-                        type='password'
-                        errMsg={errors?.password?.message || null}
-                        required:true
-                        control={control}
-                    /> 
-                    <br />
-                    <input className='submit_btn' type="submit" value="Submit" name='login_submit' disabled={loading}/>
-                </div>
-                <div className="sign_up_p">
-                    <h5 onClick={()=> setCurrentView('forgotPassowrd')}>Forgot Password?</h5>
-                    <p>Don't have an account? <span onClick={()=> setCurrentView('signup')}>Sign Up</span></p>
-                </div>
-            </form>
-        </div>    
-  )
-}
- 
-export default Login
+    <div className="login_box">
+
+      <form action="" method="post" onSubmit={handleSubmit(login)}>
+      <p>Please enter your details to Sign In</p>
+
+        <div className="form_body">
+
+          <label htmlFor="email">
+            Email<span>*</span>
+          </label>
+          <br />
+          <TextInputComponent
+            name="email"
+            errMsg={errors?.email?.message || null}
+            required:true
+            control={control}
+          />
+          <br />
+          <label htmlFor="password">
+            Password<span>*</span>
+          </label>
+          <br />
+          <TextInputComponent
+            name="password"
+            type="password"
+            errMsg={errors?.password?.message || null}
+            required:true
+            control={control}
+          />
+          <br />
+          <input
+            className="submit_btn"
+            type="submit"
+            value="Sign In"
+            name="login_submit"
+            disabled={loading}
+          />
+        </div>
+        <div className="sign_up_p">
+          <h5 onClick={() => setCurrentView("forgotPassowrd")}>
+            Forgot Password?
+          </h5>
+          <p>
+            Don't have an account?{" "}
+            <span onClick={() => setCurrentView("signup")}>Sign Up</span>
+          </p>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
--- frontend/src/pages/LoginPage/LoginPage.jsx
+++ frontend/src/pages/LoginPage/LoginPage.jsx
@@ -1,106 +1,105 @@
-import React, { useContext, useEffect, useState } from 'react'
-import './LoginPage.css'
-import Login from '../../Components/Login/Login'
-import SignUp from '../../Components/SignUp/SignUp'
-import {useLocation, useNavigate} from 'react-router-dom'
-import ForgotPassword from '../../Components/ForgotPassword/ForgotPassword'
-import Token from '../../Components/Token/Token'
-import GeneratePassword from '../../Components/GeneratePassword/GeneratePassword'
-import { useSelector } from 'react-redux'
+import React, { useContext, useEffect, useState } from "react";
+import "./LoginPage.css";
+import Login from "../../Components/Login/Login";
+import SignUp from "../../Components/SignUp/SignUp";
+import { useLocation, useNavigate } from "react-router-dom";
+import ForgotPassword from "../../Components/ForgotPassword/ForgotPassword";
+import Token from "../../Components/Token/Token";
+import GeneratePassword from "../../Components/GeneratePassword/GeneratePassword";
+import { useSelector } from "react-redux";
 // import logo from '../../../assets/images/logo-png'
-import main_logo from '../../assets/images/Maya-logo.png'
-import { Link } from 'react-router-dom'
-
+import main_logo from "../../assets/images/Maya-logo.png";
+import { Link } from "react-router-dom";
+import cover__pic from "../../assets/images/desktopImage_2.jpg";
+import logoo from "../../assets/images/logo.png"
 
 const LoginPage = () => {
   const location = useLocation();
-  const [currentView,setCurrentView] = useState('login')
+  const [currentView, setCurrentView] = useState("login");
   let [loggedIn, setLoggedIn] = useState(false);
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
-  useEffect(()=>{
-    if(location.state && location.state.currentView){
-      setCurrentView(location.state.currentView)
+  useEffect(() => {
+    if (location.state && location.state.currentView) {
+      setCurrentView(location.state.currentView);
     }
-  },[location.state])
+  }, [location.state]);
 
-  const renderComponent = ()=>{
-    switch (currentView){
-      case 'login':
-        return <Login setCurrentView = {setCurrentView} setLoggedIn= {setLoggedIn}/>
-      case 'signup':
-        return <SignUp setCurrentView = {setCurrentView} />
-      case 'forgotPassowrd':
-        return <ForgotPassword setCurrentView = {setCurrentView} />
-      case 'token':
-        return <Token setCurrentView={setCurrentView} />
-      case 'generatePassword':
-        return <GeneratePassword setCurrentView={setCurrentView} />  
-      default :
-      return <Login setCurrentView = {setCurrentView}  setLoggedIn= {setLoggedIn}/>
+  const renderComponent = () => {
+    switch (currentView) {
+      case "login":
+        return (
+          <Login setCurrentView={setCurrentView} setLoggedIn={setLoggedIn} />
+        );
+      case "signup":
+        return <SignUp setCurrentView={setCurrentView} />;
+      case "forgotPassowrd":
+        return <ForgotPassword setCurrentView={setCurrentView} />;
+      case "token":
+        return <Token setCurrentView={setCurrentView} />;
+      case "generatePassword":
+        return <GeneratePassword setCurrentView={setCurrentView} />;
+      default:
+        return (
+          <Login setCurrentView={setCurrentView} setLoggedIn={setLoggedIn} />
+        );
     }
-  }
- 
-
-
+  };
 
   // from redux
-  const loggedInUser = useSelector((root)=>{
-    return root.user.loggedInUser || null
-  })
+  const loggedInUser = useSelector((root) => {
+    return root.user.loggedInUser || null;
+  });
 
-  useEffect(()=>{
-    if(loggedInUser){
-      if(loggedInUser.role === "admin"){         
-        navigate('/admin')  
-        setLoggedIn(false)         
-      }else{
-        setLoggedIn(true)
+  useEffect(() => {
+    if (loggedInUser) {
+      if (loggedInUser.role === "admin") {
+        navigate("/admin");
+        setLoggedIn(false);
+      } else {
+        setLoggedIn(true);
       }
     }
-    
-  },[loggedInUser])
-
-
+  }, [loggedInUser]);
 
   return loggedIn ? (
     <>
       <div className={`login_page `}>
-          Your Profile
-          <div className="cross_btn">
-              <span>
-                <svg
-                  style={{ enableBackground: "new 0 0 24 24" }}
-                  version="1.1"
-                  width="30px"
-                  height="30px"
-                  viewBox="0 0 24 24"
-                  xmlns="http://www.w3.org/2000/svg"
-                  xmlnsXlink="http://www.w3.org/1999/xlink"
-                >
-                  <style>{`.st0{opacity:0.2;fill:none;stroke:#ffffff;stroke-width:0.05;stroke-miterlimit:10;}`}</style>
-                  <g id="grid_system" />
-                  <g id="_icons">
-                    <path
-                      d="M8.3,15.7C8.5,15.9,8.7,16,9,16s0.5-0.1,0.7-0.3l2.3-2.3l2.3,2.3c0.2,0.2,0.5,0.3,0.7,0.3s0.5-0.1,0.7-0.3c0.4-0.4,0.4-1,0-1.4L13.4,12l2.3-2.3c0.4-0.4,0.4-1,0-1.4s-1-0.4-1.4,0L12,10.6L9.7,8.3c-0.4-0.4-1-0.4-1.4,0s-0.4,1,0,1.4l2.3,2.3l-2.3,2.3C7.9,14.7,7.9,15.3,8.3,15.7z"
-                    />
-                    <path
-                      d="M12,21c5,0,9-4,9-9s-4-9-9-9s-9,4-9,9S7,21,12,21z M12,5c3.9,0,7,3.1,7,7s-3.1,7-7,7s-7-3.1-7-7S8.1,5,12,5z"
-                    />
-                  </g>
-                </svg>
-              </span>
-          </div>        
+        Your Profile
+        <div className="cross_btn">
+          <span>
+            <svg
+              style={{ enableBackground: "new 0 0 24 24" }}
+              version="1.1"
+              width="30px"
+              height="30px"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+              xmlnsXlink="http://www.w3.org/1999/xlink"
+            >
+              <style>{`.st0{opacity:0.2;fill:none;stroke:#ffffff;stroke-width:0.05;stroke-miterlimit:10;}`}</style>
+              <g id="grid_system" />
+              <g id="_icons">
+                <path d="M8.3,15.7C8.5,15.9,8.7,16,9,16s0.5-0.1,0.7-0.3l2.3-2.3l2.3,2.3c0.2,0.2,0.5,0.3,0.7,0.3s0.5-0.1,0.7-0.3c0.4-0.4,0.4-1,0-1.4L13.4,12l2.3-2.3c0.4-0.4,0.4-1,0-1.4s-1-0.4-1.4,0L12,10.6L9.7,8.3c-0.4-0.4-1-0.4-1.4,0s-0.4,1,0,1.4l2.3,2.3l-2.3,2.3C7.9,14.7,7.9,15.3,8.3,15.7z" />
+                <path d="M12,21c5,0,9-4,9-9s-4-9-9-9s-9,4-9,9S7,21,12,21z M12,5c3.9,0,7,3.1,7,7s-3.1,7-7,7s-7-3.1-7-7S8.1,5,12,5z" />
+              </g>
+            </svg>
+          </span>
+        </div>
       </div>
     </>
   ) : (
     <>
+      <div className="account__photo">
+        <img src={cover__pic} alt="" className="account_cover_poto"/>
+        <p>s</p>
+        {/* <img src={logoo} alt="" className="account_user_name"/> */}
+      </div>
       <div className={`login_page`}>
-        <div className="login_overlay"></div>
-        <div className='loginform'>
+        <div className="loginform">
           <div className="loginform_box">
-            <Link to='/'>
-              <div className="cross_btn" >
+            <Link to="/">
+              <div className="cross_btn">
                 <span>
                   <svg
                     style={{ enableBackground: "new 0 0 24 24" }}
@@ -114,35 +113,27 @@ const LoginPage = () => {
                     <style>{`.st0{opacity:0.2;fill:none;stroke:#ffffff;stroke-width:0.05;stroke-miterlimit:10;}`}</style>
                     <g id="grid_system" />
                     <g id="_icons">
-                      <path
-                        d="M8.3,15.7C8.5,15.9,8.7,16,9,16s0.5-0.1,0.7-0.3l2.3-2.3l2.3,2.3c0.2,0.2,0.5,0.3,0.7,0.3s0.5-0.1,0.7-0.3c0.4-0.4,0.4-1,0-1.4L13.4,12l2.3-2.3c0.4-0.4,0.4-1,0-1.4s-1-0.4-1.4,0L12,10.6L9.7,8.3c-0.4-0.4-1-0.4-1.4,0s-0.4,1,0,1.4l2.3,2.3l-2.3,2.3C7.9,14.7,7.9,15.3,8.3,15.7z"
-                      />
-                      <path
-                        d="M12,21c5,0,9-4,9-9s-4-9-9-9s-9,4-9,9S7,21,12,21z M12,5c3.9,0,7,3.1,7,7s-3.1,7-7,7s-7-3.1-7-7S8.1,5,12,5z"
-                      />
+                      <path d="M8.3,15.7C8.5,15.9,8.7,16,9,16s0.5-0.1,0.7-0.3l2.3-2.3l2.3,2.3c0.2,0.2,0.5,0.3,0.7,0.3s0.5-0.1,0.7-0.3c0.4-0.4,0.4-1,0-1.4L13.4,12l2.3-2.3c0.4-0.4,0.4-1,0-1.4s-1-0.4-1.4,0L12,10.6L9.7,8.3c-0.4-0.4-1-0.4-1.4,0s-0.4,1,0,1.4l2.3,2.3l-2.3,2.3C7.9,14.7,7.9,15.3,8.3,15.7z" />
+                      <path d="M12,21c5,0,9-4,9-9s-4-9-9-9s-9,4-9,9S7,21,12,21z M12,5c3.9,0,7,3.1,7,7s-3.1,7-7,7s-7-3.1-7-7S8.1,5,12,5z" />
                     </g>
                   </svg>
                 </span>
               </div>
             </Link>
             <div className="loginform_box_grid">
-              {renderComponent()}
               <div className="login_message">
                 <div className="loginpage_message">
-                  <img src={main_logo} alt="" className='login__page__imgae'/>
-                  <h2>Welcome Again!</h2>
-                  <p>
-                   Please enter your details to Sign In
-                  </p>
+                  {/* <img src={main_logo} alt="" className="login__page__imgae" /> */}
+                  <h2>Hello Mayaluss !</h2>
                 </div>
               </div>
+              <div>{renderComponent()}</div>
             </div>
           </div>
         </div>
       </div>
     </>
   );
-  
-}
+};
 
-export default LoginPage
+export default LoginPage;
