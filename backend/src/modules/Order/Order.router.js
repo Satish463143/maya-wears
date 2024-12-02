506e7f606fcac47a482ad61cd50370279d6078cc
Satish <mahatosatish463@gmail.com>
2024-12-02 23:38:41 +0545

    Listed all router for order

--- backend/src/modules/Order/Order.router.js
+++ backend/src/modules/Order/Order.router.js
@@ -1,9 +1,20 @@
 const loginCheck = require('../../middlewares/auth.middlewares')
+const hasPermission = require('../../middlewares/rbac.middlewares')
 const OrderController = require('./Order.controller')
 
 const router  = require('express').Router()
 
 router.route('/')
-    .post(loginCheck, OrderController.create)
+    .post( OrderController.create) // user create order
+    .get(loginCheck,hasPermission('admin'), OrderController.index ) //list all order for admin
+
+router.get('/listForUser', OrderController.index ) // list all order for user 
+router.put('/:id/cancel', OrderController.updateForUser) // update for user
+
+roter.route('/:id')
+    .get(loginCheck, hasPermission('admin'), OrderController.show) // get order details by id for admin
+    .put(loginCheck, hasPermission('admin'), OrderController.update) // get order details by id for admin
+
+
 
 module.exports = router
