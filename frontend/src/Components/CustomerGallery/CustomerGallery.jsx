7bbba1bdf7400204f601d3fde35518e418af3c63
Satish <mahatosatish463@gmail.com>
2024-12-14 22:19:43 +0545

    solved minor problem

--- frontend/src/Components/CustomerGallery/CustomerGallery.jsx
+++ frontend/src/Components/CustomerGallery/CustomerGallery.jsx
@@ -8,24 +8,20 @@ import three from "../../assets/images/three.jpg";
 import four from "../../assets/images/four.jpg";
 import five from "../../assets/images/five.jpg";
 import six from "../../assets/images/six.jpg";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 function CustomerGallery() {
   const images = [one, two, three, four, five, six];
-  const [currentIndex, setCurrentIndex] = useState(0);
-
-  // Handlers for navigation
-  const handleNext = () => {
-    setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
-  };
-
-  const handlePrev = () => {
-    setCurrentIndex((prevIndex) =>
-      prevIndex === 0 ? images.length - 1 : prevIndex - 1
-    );
-  };
-
-  const handleDotClick = (index) => {
-    setCurrentIndex(index);
+  const settings = {
+    infinite: true, // Infinite scrolling
+    speed: 500, // Animation speed
+    slidesToShow: 1, // Show 4 elements at a time
+    slidesToScroll: 1, 
+    autoPlay:true,// Scroll one element at a time
+    nextArrow: <NextArrow />,
+    prevArrow: <PrevArrow />,
   };
 
   return (
@@ -37,35 +33,66 @@ function CustomerGallery() {
         <p className="add__photo__text">Add your Photo</p>
       </div>
 
-      {/* Slider */}
-      <div className="image__slider__main">
-        <div className="slider">
-          <button className="prev" onClick={handlePrev}>
-            ❮
-          </button>
-          <img
-            src={images[currentIndex]}
-            alt={`slide ${currentIndex + 1}`}
-            className="slider-image"
-          />
-          <button className="next" onClick={handleNext}>
-            ❯
-          </button>
-        </div>
+      <Slider {...settings}>
+        {images.map((item, index)=>(
+          <img key={index} src={item} alt="" />
+        ))}
+      </Slider>
 
-        {/* Dots Navigation */}
-        <div className="dots-container">
-          {images.map((_, index) => (
-            <span
-              key={index}
-              className={`dot ${index === currentIndex ? "active" : ""}`}
-              onClick={() => handleDotClick(index)}
-            ></span>
-          ))}
-        </div>
-      </div>
+     
+
     </div>
   );
 }
+
+// Custom Next Arrow
+const NextArrow = (props) => {
+  const { className, style, onClick } = props;
+  return (
+      <div
+          className={className}
+          style={{
+              ...style,
+              display: "block",
+              // background: "#5BCEFA",
+              borderRadius: "50%",
+              border:'1px solid var(--pri_text)',
+              right:'-20px',
+              width:'30px',
+              height:'30px',
+              color:'var(--pri_text)',
+              display:'flex',
+              justifyContent:'center',
+              alignItems:'center'
+          }}
+          onClick={onClick}
+      />
+  );
+};
+
+// Custom Previous Arrow
+const PrevArrow = (props) => {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{
+          ...style,
+          display: "block",
+          // background: "#5BCEFA",
+          borderRadius: "50%",
+          width:'30px',
+          left:'-20px',
+          border:'1px solid var(--pri_text)',
+          height:'30px',
+          display:'flex',
+          justifyContent:'center',
+          alignItems:'center',
+          zIndex:'99'        
+      }}
+      onClick={onClick}
+    />
+  );
+};
 
 export default CustomerGallery;
