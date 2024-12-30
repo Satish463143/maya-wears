5a765a68837e2b32b4ccc8cc228fe3da9f296f9a
Satish <mahatosatish463@gmail.com>
2024-12-30 19:39:39 +0545

    Gallery imlemented sucessfulley

--- frontend/src/Components/CustomerGallery/CustomerGallery.jsx
+++ frontend/src/Components/CustomerGallery/CustomerGallery.jsx
@@ -1,28 +1,30 @@
 import React, { useState } from "react";
 import "./CustomerGallery.css";
-
-// Import images
-import one from "../../assets/images/image2.png";
-import two from "../../assets/images/image3.png";
-import three from "../../assets/images/image4.png";
-import four from "../../assets/images/image5.png";
-import five from "../../assets/images/image6.png";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import { useListAllGalleryQuery } from "../../api/gallery.api";
+import LoadingComponent from "../../Middlewares/Loading/Loading.component";
 
 function CustomerGallery() {
-  const images = [one, two, three, four, five];
+  const [page, setPage] = useState(1);
+  const [limit] = useState(10); 
+  const {data,error,isLoading} = useListAllGalleryQuery({page, limit,})
+
+  console.log("API response:", data);
+    if(isLoading){
+        return <LoadingComponent/>
+    }
+    const galleries = data?.result?.allImages || [];
+
 
   return (
     <div className="div_container galleryyy">
-      {/* Slider Header */}
       <div className="slider__name">
         <h1>#MayaNaiMaya</h1>
         <p className="share__text">Share your style with Maya</p>
-        <p className="add__photo__text">Add your Photo</p>
       </div>
       <div className="cus__gallery">
-        {images.map((item, index) => (
+        {galleries.map((item, index) => (
           <img key={index} src={item} alt="" />
         ))}
       </div>
