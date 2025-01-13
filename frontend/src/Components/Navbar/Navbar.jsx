d4ee6dba16353207883d4f34779c0d66cbcf5a51
Aarush Mahato <aarushmahato4@gmail.com>
2025-01-13 14:40:58 +0545

    #CHANGES

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -4,7 +4,7 @@ import { Link, useLocation, useNavigate } from "react-router-dom";
 import logos from "../../assets/images/logo-1.png";
 import search_logo from "../../assets/images/search_icon.svg";
 
-const Navbar = ({toogleCart}) => {
+const Navbar = ({ toogleCart }) => {
   const [isMenuActive, setIsMenuActive] = useState(false);
   const [isSearchActive, setIsSearchActive] = useState(false);
   const [lastScrollY, setLastScrollY] = useState(0);
@@ -12,28 +12,32 @@ const Navbar = ({toogleCart}) => {
   const [menu, setMenu] = useState("Home");
   const [navbarBackground, setNavbarBackground] = useState(false);
   const location = useLocation();
-  const navigate = useNavigate()
-  const [search, setSearch] = useState('');
+  const navigate = useNavigate();
+  const [search, setSearch] = useState("");
 
   const toggleNav = () => {
-    setIsMenuActive(!isMenuActive);    
-    if (!isMenuActive) {
+    setIsMenuActive(!isMenuActive);
+    if (isMenuActive) {
       document.body.classList.add("no-scroll");
     } else {
       document.body.classList.remove("no-scroll");
     }
   };
-  const toggleSearch = ()=>
-  {
-    setIsSearchActive(!isSearchActive)
+  if (isSearchActive) {
+    document.body.classList.add("no-scroll");
+  } else {
+    document.body.classList.remove("no-scroll");
   }
+  const toggleSearch = () => {
+    setIsSearchActive(!isSearchActive);
+  };
   const handleSearchChange = (event) => {
     setSearch(event.target.value);
   };
 
   const handleSearchSubmit = (event) => {
-    if (event.key === 'Enter') {
-      navigate('/all_product', { state: { searchQuery: search.trim() } });
+    if (event.key === "Enter") {
+      navigate("/all_product", { state: { searchQuery: search.trim() } });
     }
   };
 
@@ -69,26 +73,19 @@ const Navbar = ({toogleCart}) => {
       setMenu("Home");
     } else if (path === "/about") {
       setMenu("About");
-    }
-    else if (path === "/contact") {
+    } else if (path === "/contact") {
       setMenu("Contact");
-    }
-    else if (path === "/about_us") {
+    } else if (path === "/about_us") {
       setMenu("About Us");
-    }
-    else if (path === "/all_product") {
+    } else if (path === "/all_product") {
       setMenu("Products");
-    }
-    else if (path === "/gallery") {
+    } else if (path === "/gallery") {
       setMenu("Gallery");
-    }
-    else if (path === "/product/:slug/:id") {
+    } else if (path === "/product/:slug/:id") {
       setMenu("Products");
-    }
-    else if (path === "/my_account") {
+    } else if (path === "/my_account") {
       setMenu("My Accounts");
-    }
-     else {
+    } else {
       setMenu("");
     }
   }, [location]);
@@ -119,45 +116,41 @@ const Navbar = ({toogleCart}) => {
                 </Link>
               </div>
               <div className="end_menu">
-                <ul>                  
-                    <li className="to_hide_nav">
-                      <img src={search_logo} alt="" style={{height:'25px', cursor:'pointer'}} onClick={toggleSearch}/>
-                        <div className={`search_box ${isSearchActive && 'active_search_box'}`}>
-                          <input
-                            type="text"
-                            placeholder="Search product..."
-                            value={search}
-                            onChange={handleSearchChange}
-                            onKeyDown={handleSearchSubmit} // Handle Enter key press
-                          />
-                        </div>                      
-                    </li>
-                  <Link to="/login">
-                  <li style={{ cursor: "pointer" }}>
-                    <svg
-                      viewBox="0 0 32 32"
-                      height="25px"
-                      width="25px"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <title />
-                      <g
-                        data-name="user people person users man"
-                        id="user_people_person_users_man"
-                      >
-                        <path
-                          d="M23.74,16.18a1,1,0,1,0-1.41,1.42A9,9,0,0,1,25,24c0,1.22-3.51,3-9,3s-9-1.78-9-3a9,9,0,0,1,2.63-6.37,1,1,0,0,0,0-1.41,1,1,0,0,0-1.41,0A10.92,10.92,0,0,0,5,24c0,3.25,5.67,5,11,5s11-1.75,11-5A10.94,10.94,0,0,0,23.74,16.18Z"
-                          stroke="currentColor"
-                          strokeWidth="0."
-                        />
-                        <path
-                          d="M16,17a7,7,0,1,0-7-7A7,7,0,0,0,16,17ZM16,5a5,5,0,1,1-5,5A5,5,0,0,1,16,5Z"
-                          stroke="currentColor"
-                          strokeWidth="0."
-                        />
-                      </g>
-                    </svg>
+                <ul>
+                  <li className="to_hide_nav">
+                    <img
+                      src={search_logo}
+                      alt=""
+                      style={{ height: "25px", cursor: "pointer" }}
+                      onClick={toggleSearch}
+                    />
                   </li>
+                  <Link to="/login">
+                    <li style={{ cursor: "pointer" }}>
+                      <svg
+                        viewBox="0 0 32 32"
+                        height="25px"
+                        width="25px"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <title />
+                        <g
+                          data-name="user people person users man"
+                          id="user_people_person_users_man"
+                        >
+                          <path
+                            d="M23.74,16.18a1,1,0,1,0-1.41,1.42A9,9,0,0,1,25,24c0,1.22-3.51,3-9,3s-9-1.78-9-3a9,9,0,0,1,2.63-6.37,1,1,0,0,0,0-1.41,1,1,0,0,0-1.41,0A10.92,10.92,0,0,0,5,24c0,3.25,5.67,5,11,5s11-1.75,11-5A10.94,10.94,0,0,0,23.74,16.18Z"
+                            stroke="currentColor"
+                            strokeWidth="0."
+                          />
+                          <path
+                            d="M16,17a7,7,0,1,0-7-7A7,7,0,0,0,16,17ZM16,5a5,5,0,1,1-5,5A5,5,0,0,1,16,5Z"
+                            stroke="currentColor"
+                            strokeWidth="0."
+                          />
+                        </g>
+                      </svg>
+                    </li>
                   </Link>
                 </ul>
               </div>
@@ -187,37 +180,45 @@ const Navbar = ({toogleCart}) => {
                   </li>
                 </Link>
                 <Link to="/all_product">
-                  <li onClick={() => {
+                  <li
+                    onClick={() => {
                       toggleNav();
                       setMenu("Products");
                     }}
                     className={menu === "Products" ? "liActive" : ""}
-                    
-                    style={{ background: "none" }}>
+                    style={{ background: "none" }}
+                  >
                     All Products
                   </li>
                 </Link>
                 <Link to="/gallery">
-                  <li onClick={() => {
+                  <li
+                    onClick={() => {
                       toggleNav();
                       setMenu("Gallery");
-                    }} 
+                    }}
                     className={menu === "Gallery" ? "liActive" : ""}
-                    
-                    style={{ background: "none" }}>
+                    style={{ background: "none" }}
+                  >
                     Gallery
                   </li>
-                </Link>              
+                </Link>
                 <Link>
-                  <li onClick={()=> { toogleCart(); toggleNav(); setMenu("Cart");}} 
+                  <li
+                    onClick={() => {
+                      toogleCart();
+                      toggleNav();
+                      setMenu("Cart");
+                    }}
                     className={menu === "Cart" ? "liActive" : ""}
-                  
-                  style={{ background: "none" }}>
+                    style={{ background: "none" }}
+                  >
                     Cart
                   </li>
                 </Link>
-                <Link to='/contact'>
-                  <li onClick={() => {
+                <Link to="/contact">
+                  <li
+                    onClick={() => {
                       toggleNav();
                       setMenu("Contact");
                     }}
@@ -253,6 +254,24 @@ const Navbar = ({toogleCart}) => {
                 </Link>
               </ul>
             </nav>
+          </div>
+          <div
+            className={`search_overlay ${
+              isSearchActive && "active_search_overlay"
+            }`}
+          >
+            <div
+              className={`search_box ${isSearchActive && "active_search_box"}`}
+            >
+              <input
+                type="text"
+                placeholder="Search product..."
+                value={search}
+                onChange={handleSearchChange}
+                onKeyDown={handleSearchSubmit} // Handle Enter key press
+              />
+              <p onClick={toggleSearch} className="close_btnnn" >Close</p>
+            </div>
           </div>
         </div>
       </header>
